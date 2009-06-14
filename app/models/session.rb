9350d74187cccf7d15d07684787441bc5fa669f9
Alex Buijs <alex.buijs@ciber.nl>
2009-06-14 23:51:51 +0200

    Update annotations

--- app/models/session.rb
+++ app/models/session.rb
@@ -5,16 +5,16 @@
 #
 #  id                         :integer         not null, primary key
 #  sender_id                  :integer(0)      not null
-#  max_sender_selection_time  :integer(0)      not null
-#  sender_focus_time          :integer(0)      not null
-#  max_receiver_response_time :integer(0)      not null
-#  recovery_time              :integer(0)      not null
+#  max_sender_selection_time  :integer(0)      default(10), not null
+#  sender_focus_time          :integer(0)      default(30), not null
+#  max_receiver_response_time :integer(0)      default(30), not null
+#  recovery_time              :integer(0)      default(15), not null
 #  trial_size                 :integer(0)      not null
 #  sample_size                :integer(0)      not null
 #  genre                      :string(0)       not null
 #  type                       :string(0)       not null
 #  genre_type_ratio           :decimal(, )     not null
-#  sampling_with_replacement  :boolean         not null
+#  sampling_with_replacement  :boolean         default(TRUE), not null
 #  created_at                 :datetime        not null
 #  updated_at                 :datetime        not null
 #
